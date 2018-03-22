98d629a1f402b4c9c50c5146d36c3630d2ce1d6c
Tobias Kunze <rixx@cutebit.de>
2018-03-22 21:42:17 +0100

    [server] Add tests for list views

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,8 +1,47 @@
-import pytest
+from datetime import timedelta
 
-from eventomat.event.models import Room
+import pytest
+from django.contrib.auth import get_user_model
+from django.utils.timezone import now
+
+from eventomat.event.models import Event, Room, Series
+
+
+@pytest.fixture
+def keyholder_user():
+    return get_user_model().objects.create(username='keyholderrr')
 
 
 @pytest.fixture
 def room():
     return Room.objects.create(name='The Rom')
+
+
+@pytest.fixture
+def series():
+    return Series.objects.create(name='Best series')
+
+
+@pytest.fixture
+def event_without_series(room, keyholder_user):
+    return Event.objects.create(
+        name='Teh Event',
+        start=now() + timedelta(days=1),
+        end=now() + timedelta(hours=25),
+        publish=True,
+        keyholder=keyholder_user,
+        room=room
+    )
+
+
+@pytest.fixture
+def event_with_series(room, keyholder_user, series):
+    return Event.objects.create(
+        name='Teh Event',
+        start=now() + timedelta(days=1),
+        end=now() + timedelta(hours=25),
+        publish=True,
+        keyholder=keyholder_user,
+        series=series,
+        room=room
+    )
--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -1,6 +1,7 @@
 import json
 
 import pytest
+import pytz
 
 
 @pytest.mark.django_db()
@@ -9,4 +10,42 @@ def test_room_list(room, client):
     assert response.status_code == 200
     content = json.loads(response.content.decode())
     assert len(content) == 1, content
-    assert content[0] == {'id': room.id, 'name': room.name}
+    assert content[0] == {
+        'id': room.id,
+        'name': room.name,
+    }
+
+
+@pytest.mark.django_db()
+def test_event_list(event_with_series, client):
+    event = event_with_series
+    response = client.get('/events/')
+    assert response.status_code == 200
+    content = json.loads(response.content.decode())
+    assert len(content) == 1, content
+    assert content[0] == {
+        'id': event.id,
+        'name': event.name,
+        'description': event.description,
+        'start': event.start.astimezone(pytz.timezone('Europe/Berlin')).isoformat(),
+        'end': event.end.astimezone(pytz.timezone('Europe/Berlin')).isoformat(),
+        'publish': event.publish,
+        'series': {'id': event.series.id, 'name': event.series.name, 'room': None, 'start': None, 'end': None},
+        'keyholder': event.keyholder.id,
+        'room': {'id': event.room.id, 'name': event.room.name},
+    }
+
+
+@pytest.mark.django_db()
+def test_series_list(series, client):
+    response = client.get('/series/')
+    assert response.status_code == 200
+    content = json.loads(response.content.decode())
+    assert len(content) == 1, content
+    assert content[0] == {
+        'id': series.id,
+        'name': series.name,
+        'start': None,
+        'end': None,
+        'room': None,
+    }
