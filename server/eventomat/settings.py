981c1a64feb7ae9bd4cdf65ef47db048de1e1b6a
Tobias Kunze <rixx@cutebit.de>
2018-04-05 19:41:04 +0200

    [server] Add pesky fs settings

--- server/eventomat/settings.py
+++ server/eventomat/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 
 # CORS_ORIGIN_WHITELIST = []
 CORS_ORIGIN_ALLOW_ALL = True
